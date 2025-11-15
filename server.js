d3bfde9e5c8aac5264c34415d511409d11503630
msmacalopez <msmacalopezs@gmail.com>
2025-11-16 10:54:03 +1100

    server js modified

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 //1) import express
 import express from "express";
 
-//2) define express
+//2) define express varivle and execute the express
 const app = express();
-const port = 8000;
+const PORT = 8000;
 
 //4) To handle requests
-app.use("/", (req, res) => {
-  res.send(`Hi There, in port ${port}`);
+// app.use("/", (req, res) => {
+//   res.send(`Hi There, in port ${PORT}`);
+// });
+
+//5) get from Server: READ
+// app.get("/", (req, res) => {
+//   res.send("Hi");
+// });
+
+//6)Send file->do pwd
+app.get("/", (req, res) => {
+  res.sendFile(
+    "/Users/MacaLopez/Documents/DentedCode/DentedCode - Node/NewNode/home.html"
+  );
 });
 
 //3)open the port
-app.listen(port, (error) => {
-  error ? console.log(error) : console.log("http://localhost:8000/");
+app.listen(PORT, (error) => {
+  error ? console.log(error) : console.log(`http://localhost:${PORT}`);
 });
